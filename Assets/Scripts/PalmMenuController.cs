0e3f78fc744c5faf696dd11217909f40bd12f260
Phillipe Sebastiao <phillipe.sebastiao@connect.qut.edu.au>
2024-08-26 01:17:54 +1000

    [FEAT] Hand menu

---
+++ Assets/Scripts/PalmMenuController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+namespace Oculus.Interaction.Samples.PalmMenu
+{
+    public class PalmMenu : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _individualPartsEnabledIcon;
+
+        [SerializeField]
+        private GameObject _individualPartsDisabledIcon;
+
+        public ModelController modelController;
+
+        private void Start()
+        {
+            _individualPartsEnabledIcon.SetActive(modelController.IndividualParts);
+        }
+
+        private void Update()
+        {
+        }
+
+
+        public void ToggleIndividualParts()
+        {
+            modelController.ToggleIndividualParts();
+            _individualPartsEnabledIcon.SetActive(modelController.IndividualParts);
+            _individualPartsDisabledIcon.SetActive(!modelController.IndividualParts);
+        }
+    }
+}
