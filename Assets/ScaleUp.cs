3ed36370d9e4374c2302846930f207419280e963
renkwok <115599649+renkwok@users.noreply.github.com>
2024-08-26 16:49:02 +1000

    Toggle menu

--- Assets/ScaleUp.cs
+++ Assets/ScaleUp.cs
@@ -4,33 +4,53 @@ using UnityEngine;
 
 public class ScaleUp : MonoBehaviour
 {
-    // public makes sure you can access it in the game engine
-    // and we are setting it to 0.1 now
-    public float scale = 0.1f;
+    // Public variables to access in the game engine
+    public float scaleSpeed = 0.5f; // Speed of scaling
+    public float minScale = 0.01f; // Minimum scale to prevent flipping
+    public float maxScale = 0.5f; // Maximum scale
+
+    private Vector3 scaleStep; // Step to change scale each frame
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Initialize scaleStep based on scaleSpeed
+        scaleStep = new Vector3(scaleSpeed, scaleSpeed, scaleSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetKeyDown(KeyCode.Space) == true)
-        
-        //when you press the button "u" it should scale up
-        if(Input.GetKey("u"))
+        // Determine the new scale based on user input
+        Vector3 newScale = transform.localScale;
+
+        // When you press the button "u" it should start scaling up
+        if (Input.GetKey("u"))
         {
-            // this scales the object up
-            transform.localScale = transform.localScale + new Vector3(scale,scale,scale);
+            newScale += scaleStep * Time.deltaTime;
         }
 
-        //when you press the button "d" it should scale down
-        if(Input.GetKey("d"))
+        // When you press the button "d" it should start scaling down
+        if (Input.GetKey("d"))
         {
-            // this scales the object up
-            transform.localScale = transform.localScale + new Vector3(-scale,-scale,-scale);
+            newScale -= scaleStep * Time.deltaTime;
         }
-        
+
+        // Clamp the new scale to ensure it doesn't go below minScale or above maxScale
+        newScale = new Vector3(
+            Mathf.Clamp(newScale.x, minScale, maxScale),
+            Mathf.Clamp(newScale.y, minScale, maxScale),
+            Mathf.Clamp(newScale.z, minScale, maxScale)
+        );
+
+        // Apply the new scale to the transform
+        transform.localScale = newScale;
+
+        // Rotate the object when you press the button "r"
+        if (Input.GetKey("r"))
+        {
+            // Rotate the object around the Y-axis
+            transform.Rotate(Vector3.up, scaleSpeed * Time.deltaTime * 100); // Adjust rotation speed as needed
+        }
     }
 }
