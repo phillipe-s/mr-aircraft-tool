db5c8de79398aec10207491f278ae076d176acc2
Phillipe Sebastiao <phillipe.sebastiao@connect.qut.edu.au>
2024-10-07 18:42:10 +1000

    Fix/feedback (#31)
    * Increased slider size
    
    * removed grab interactions from model when toggling individual parts
    
    * Seperated UI and Model logic

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using Oculus.Interaction.Input;
+using Unity.VisualScripting.Dependencies.NCalc;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private ModelController modelController;
+    [SerializeField] private GameObject centerEyeAnchor;
+    [SerializeField] private GameObject scrollMenuPrefab;
+    [SerializeField] private GameObject scrollMenuButtonPrefab;
+    [SerializeField] private GameObject modelMenu;
+    [SerializeField] private GameObject modelMenuToggle;
+    [SerializeField] private GameObject individualPartsToggle;
+    public GameObject IndividualPartsToggle { get => individualPartsToggle; }
+    [SerializeField] private GameObject sliderPrefab;
+    private GameObject explosionSlider;
+    [SerializeField] private GameObject explosionSliderToggle;
+    private GameObject refinedPartsMenu;
+    public GameObject refinedPartsMenuToggle;
+    private List<Toggle> refinedPartToggles = new List<Toggle>();
+    private float menuDistanceInFrontOfCamera = 0.3f;
+
+    void Start()
+    {
+        modelMenu.SetActive(false);
+
+        explosionSlider = Instantiate(sliderPrefab);
+        explosionSlider.SetActive(false);
+        explosionSlider.GetComponentInChildren<Slider>().onValueChanged.AddListener(modelController.OnExplosionSliderChange);
+
+        ToggleIcons(modelMenuToggle, false);
+        ToggleIcons(individualPartsToggle, false);
+        ToggleIcons(explosionSliderToggle, false);
+        ToggleIcons(refinedPartsMenuToggle, false);
+    }
+
+    [ContextMenu("Toggle Model Menu")]
+    public void ToggleModelMenu()
+    {
+        (modelMenu.transform.position, modelMenu.transform.rotation) = GetPositionInFrontOfCamera();
+        modelMenu.SetActive(!modelMenu.activeSelf);
+        ToggleIcons(modelMenuToggle, modelMenu.activeSelf);
+    }
+
+    [ContextMenu("Toggle Explosion Slider")]
+    public void ToggleExplosionSlider()
+    {
+        (explosionSlider.transform.position, explosionSlider.transform.rotation) = GetPositionInFrontOfCamera();
+        explosionSlider.SetActive(!explosionSlider.activeSelf);
+        ToggleIcons(explosionSliderToggle, explosionSlider.activeSelf);
+    }
+
+    [ContextMenu("Toggle Refined Parts Menu For Current Model")]
+    public void ToggleRefinedPartsMenu()
+    {
+        if (modelController.CurrentModel.RefinedParts == null) return;
+
+        if (refinedPartsMenu == null)
+        {
+            refinedPartsMenu = Instantiate(scrollMenuPrefab);
+            (refinedPartsMenu.transform.position, refinedPartsMenu.transform.rotation) = GetPositionInFrontOfCamera();
+            refinedPartsMenu.SetActive(false);
+
+            // Populate the refined parts menu with buttons
+            foreach (Model refinedPart in modelController.CurrentModel.RefinedParts)
+            {
+                Transform content = refinedPartsMenu.transform.Find("Unity Canvas/LeftSide/Scroll View/Viewport/Content");
+                content.GetComponent<ToggleGroup>().allowSwitchOff = true;
+
+                GameObject button = Instantiate(scrollMenuButtonPrefab, content);
+                button.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = refinedPart.modelName;
+                Toggle toggle = button.GetComponent<Toggle>();
+                toggle.group = content.GetComponent<ToggleGroup>();
+                toggle.onValueChanged.AddListener((value) =>
+                {
+                    modelController.ToggleRefinedPart(refinedPart, value);
+                });
+                refinedPartToggles.Add(toggle);
+            }
+            refinedPartsMenu.SetActive(true);
+            ToggleIcons(refinedPartsMenuToggle, true);
+        }
+        else
+        {
+            foreach (Toggle toggle in refinedPartToggles)
+            {
+                toggle.onValueChanged.RemoveAllListeners();
+            }
+            refinedPartToggles.Clear();
+
+            Destroy(refinedPartsMenu);
+            refinedPartsMenu = null;
+            ToggleIcons(refinedPartsMenuToggle, false);
+        }
+    }
+
+    public void ToggleIcons(GameObject toggle, bool active)
+    {
+        toggle.transform.Find("IconOn").gameObject.SetActive(active);
+        toggle.transform.Find("IconOff").gameObject.SetActive(!active);
+    }
+
+    private (Vector3, Quaternion) GetPositionInFrontOfCamera()
+    {
+        Vector3 cameraForward = centerEyeAnchor.transform.forward;
+        Vector3 cameraPosition = centerEyeAnchor.transform.position;
+        Quaternion cameraRotation = centerEyeAnchor.transform.rotation;
+        return (cameraPosition + cameraForward * menuDistanceInFrontOfCamera, cameraRotation);
+    }
+
+}
